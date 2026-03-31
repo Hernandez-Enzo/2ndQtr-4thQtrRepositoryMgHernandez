8f811fab2f94583ab88e5462fb5fb19dce06465b
Hernandez-Enzo <b2029ejahernandez@pshs.edu.ph>
2026-03-31 21:29:00 +0800

    done!

--- public/Q4.3rdGraded/script.js
+++ public/Q4.3rdGraded/script.js
@@ -1,25 +1,30 @@
 function plotPoint() {
+    let x0 = parseInt(document.getElementById('x0').value);
+    let y0 = parseInt(document.getElementById('y0').value);
+    let x = parseInt(document.getElementById('x').value);
+    let y = parseInt(document.getElementById('y').value);
     console.log(x0, y0, x, y) // checks the arguments passed to this function
-    in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
-    var point = document.createElement('div');  
+    document.getElementById('in1').innerHTML = `${x0} ${y0} ${x} ${y}`;
+    var point = document.createElement('div');
     point.className = 'point';
     /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.styleleft = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.stylebottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    point.style.setProperty('left', (x - x0 + 200 - 5) + 'px'); // subtract half the width of the point to center it by changing left css property
+    point.style.setProperty('bottom', (y - y0 + 200 - 5) + 'px'); // subtract half the height of the point to center it by changing bottom css property
     document.getElementById('coordinatePlane').appendChild(point);
 
     if (x > x0 && y > y0){
-      out1.innerHTML = "NE"
+      document.getElementById('out1').innerHTML = "NE"
     }
     else if (x < x0 && y < y0){
-      out1.innerHTML = "SO"
+      document.getElementById('out1').innerHTML = "SO"
     }
     else if (x > x0 && y < y0){
-      out1.innerHTML = "SE"
+      document.getElementById('out1').innerHTML = "SE"
     }
     else if (x < x0 && y > y0){
-      out1.innerHTML = "NO"
+      document.getElementById('out1').innerHTML = "NO"
     }
     else{
-      out1.innerHTML = "divisa"
+      document.getElementById('out1').innerHTML = "divisa"
     }
+  }
